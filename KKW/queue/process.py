3f12ea67bd5232b526b6c69ba56ab5e68f9d5ea0
Kanu <kanu7546@gmail.com>
2023-11-09 03:54:48 +0900

    Update process.py

--- KKW/queue/process.py
+++ KKW/queue/process.py
@@ -7,7 +7,7 @@ def solution(priorities, location):
     while True:
         check = que.popleft()
         location-=1
-        if len(que)>0 and check < max(que):
+        if que and check < max(que):
             que.append(check)
             if location == -1:
                 location = len(que)-1
@@ -17,3 +17,16 @@ def solution(priorities, location):
                 break
             
     return answer
+
+def solution2(priorities, location):
+    que = [(data,i) for i,data in enumerate(priorities)]
+    answer = 0
+
+    while True:
+        cnt_data = que.pop(0)
+        if que and cnt_data[0] < max(que)[0]:
+            que.append(cnt_data)
+        else:
+            answer+=1
+            if cnt_data[1] == location:
+                return answer
